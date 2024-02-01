09065b295f530ff79f36404501cd2baff835ef22
Yurii <131679817+havr1985@users.noreply.github.com>
2024-02-01 21:17:30 +0200

    start routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import { GlobalStyle } from './GlobalStyle.js'
 import { ThemeProvider } from 'styled-components'
 import { Conteiner } from './shared/Container/Conteiner.js'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={{}}>
-      <Conteiner>
-        <App />
-      </Conteiner>
+      <BrowserRouter basename='/TaskPro-blended1-frontend'>
+        <Conteiner>
+          <App />
+        </Conteiner>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>
