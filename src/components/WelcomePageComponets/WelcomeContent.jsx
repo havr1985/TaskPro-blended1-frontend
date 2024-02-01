3a90bedf163c98c3a1a82d35c2d3fa679f500b33
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2024-02-01 21:55:09 +0200

    comit

--- src/components/WelcomePageComponets/WelcomeContent.jsx
+++ src/components/WelcomePageComponets/WelcomeContent.jsx
@@ -1,5 +1,30 @@
+import {
+  WelcomeWrapper,
+  WelcomeLogo,
+  WelcomeContainer,
+  WelcomeDescription,
+  WelcomeLoginButton,
+  WelcomeRegisterButton,
+  WelcomeLogoTitle,
+} from "./WelcomeContent.styled";
+
 export const WelcomeContent = () => {
-    return (
-        <div>Welcome content</div>
-    )
-}
+  return (
+    <WelcomeWrapper>
+      <WelcomeContainer>
+        <WelcomeLogo>
+          <WelcomeLogoTitle>Task Pro</WelcomeLogoTitle>
+        </WelcomeLogo>
+        <WelcomeDescription>
+          Supercharge your productivity and take control of your tasks with Task
+          Pro - Don``t wait, start achieving your goals now!
+        </WelcomeDescription>
+
+        <WelcomeRegisterButton>Registration</WelcomeRegisterButton>
+        <WelcomeLoginButton>Log In</WelcomeLoginButton>
+      </WelcomeContainer>
+    </WelcomeWrapper>
+  );
+};
+
+export default WelcomeContent;
