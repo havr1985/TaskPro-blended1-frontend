87fe7b6fe685241fc740ff40a780b4ad3e08eb5e
Yurii <131679817+havr1985@users.noreply.github.com>
2024-02-02 08:29:00 +0200

    modal

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GlobalStyle } from './GlobalStyle.js'
 import { ThemeProvider } from 'styled-components'
-import { Conteiner } from './shared/Container/Conteiner.js'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={{}}>
       <BrowserRouter basename='/TaskPro-blended1-frontend'>
-        <Conteiner>
           <App />
-        </Conteiner>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
