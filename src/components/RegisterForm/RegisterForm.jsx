ccb0777b9ad8a94057cb5e4530e2dfb1748ddc5f
AndrewOPP <a.vyblov2003@gmail.com>
2024-02-02 20:02:39 +0200

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,21 +1,3 @@
-import { useState } from "react";
-import { EditProfileModal } from "../EditProfileModal/EditProfileModal";
-
 export const RegisterForm = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const modalStateSwapper = () => {
-    setIsModalOpen((prevState) => !prevState);
-  };
-  // console.log(isModalOpen);
-  return (
-    <>
-      <button onClick={modalStateSwapper}>Open modal</button>
-      <EditProfileModal
-        isModalOpen={isModalOpen}
-        modalStateSwapper={modalStateSwapper}
-      />
-    </>
-  );
-  // return <div>Welcome content</div>;
+  return <div>Welcome content</div>;
 };
