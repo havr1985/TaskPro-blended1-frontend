e485b617cf699a719feaedc05aa72bccb8b36db8
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2024-02-03 00:46:22 +0200

    відкривається сама не відпрацьовує закриття

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,3 +1,13 @@
+import { useModal } from "../../hooks/useModal";
+import { AddBoardModal } from "../BoardModal/newBoardModal/newBoardModal";
+
 export const RegisterForm = () => {
-  return <div>Welcome content</div>;
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  return (
+    <>
+      <button onClick={openModal}>Open modal</button>
+      <AddBoardModal isModalOpen={isModalOpen} onCloseModal={closeModal} />
+    </>
+  );
 };
