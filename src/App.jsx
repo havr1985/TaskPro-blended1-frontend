2e1738c3339e20c7608d26931c4730fa3ed97457
Yurii <131679817+havr1985@users.noreply.github.com>
2024-02-03 09:32:22 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 import { lazy } from "react";
 import AuthPage from "./pages/AuthPage";
-import ScreensPage from "./pages/ScreensPage";
+
 
 const WelcomePage = lazy(() => import("./pages/WelcomePage"));
+const HomePage = lazy(() => import('./pages/HomePage'))
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<WelcomePage />} />
         <Route path="auth/:id" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage/> } />
       </Route>
     </Routes>
   );
