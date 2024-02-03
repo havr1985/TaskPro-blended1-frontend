1c07b205491be9b6245f6f6604c56819b4038841
Pavel Nedoshyvko <plight83@gmail.com>
2024-02-03 02:46:02 +0200

    add screens-page

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -36,6 +36,27 @@ html {
   box-sizing: inherit;
 }
 
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+ul {
+  margin: 0;
+	padding: 0;
+	list-style: none;
+}
+
+a {
+  color: currentColor;
+  text-decoration: none;
+}
+
 img {
   display: block;
   max-width: 100%;
---
+++ src/components/Filters/Filters.styled.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const FilterButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  width: 64px;
+  height: 20px;
+  padding: 0;
+  font-weight: 500;
+  font-size: 14px;
+  letter-spacing: -0.02em;
+  color: rgba(255, 255, 255, 0.8);
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const FilterIcon = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: none;
+  stroke-width: 1.5px;
+  stroke: rgba(255, 255, 255, 0.8);
+`;
