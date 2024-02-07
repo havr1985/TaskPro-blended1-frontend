8a34349791e367f2501b7b67bebc03929a1da995
AndrewOPP <a.vyblov2003@gmail.com>
2024-02-07 13:36:53 +0200

    header avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,21 @@ import {
   MenuButtonContainer,
   MenuIcon,
   ThemeAndUserContainer,
+  UserAvatar,
   UserContainer,
   UserIcon,
+  UsernameText,
 } from "./Header.styled";
 import sprite from "../../shared/images/icons.svg";
 import { ThemeComponent } from "../ThemeMode/ThemeMenu";
 import { EditProfileModal } from "../EditProfileModal/EditProfileModal";
 import { useState } from "react";
+import { selectAuthUserData } from "../../redux/Auth/authSelectors";
+import { useSelector } from "react-redux";
 
 export const Header = ({ openSideBar }) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-
+  const { email, username, avatarURL } = useSelector(selectAuthUserData);
   const modalStateSwapper = () => {
     setIsEditModalOpen((prev) => !prev);
   };
@@ -30,10 +34,19 @@ export const Header = ({ openSideBar }) => {
       <ThemeAndUserContainer>
         <ThemeComponent />
         <UserContainer onClick={() => modalStateSwapper()}>
-          <p>User</p>
-          <UserIcon className="iconLightning" width="12px" height="16px">
-            <use href={`${sprite}#icon-user`}></use>
-          </UserIcon>
+          <UsernameText>{username}</UsernameText>
+          {avatarURL ? (
+            <UserAvatar
+              src={avatarURL}
+              alt="avatar"
+              width="32px"
+              height="32px"
+            />
+          ) : (
+            <UserIcon className="iconLightning" width="12px" height="16px">
+              <use href={`${sprite}#icon-user`}></use>
+            </UserIcon>
+          )}
         </UserContainer>
       </ThemeAndUserContainer>
       <EditProfileModal
