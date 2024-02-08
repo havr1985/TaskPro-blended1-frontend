a164e4b7adc767cd3d1c44d813158d4595a1e3a5
Oleh <oleh.tsarevych@gmail.com>
2024-02-08 14:03:34 +0200

    add dispatch current boards, fix select board

--- src/components/Sidebar/MyBoards/MyBoards.jsx
+++ src/components/Sidebar/MyBoards/MyBoards.jsx
@@ -2,109 +2,84 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../../hooks/useModal";
 import { AddBoardModal } from "../../BoardModal/newBoardModal/newBoardModal";
 import {
-  BoardContainer,
-  BoardIcon,
-  BoardItem,
-  BoardList,
-  BoardTitle,
-  ButtonContainer,
-  ButtonCreateBoard,
-  ButtonEdit,
-  CreateBoardWrapper,
-  Heading,
+	BoardContainer,
+	BoardIcon,
+	BoardItem,
+	BoardList,
+	BoardTitle,
+	ButtonContainer,
+	ButtonCreateBoard,
+	ButtonEdit,
+	CreateBoardWrapper,
+	Heading,
+	Link,
 } from "./MyBoards.styled";
 import { deleteDashboardThunk } from "../../../redux/Dashboard/dashboardOperation";
-import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import { EditBoardModal } from "../../BoardModal/EditBoardModal/editBoardModal";
 
-export const MyBoards = ({
-  icon,
-  boards,
-  selectedItem,
-  choice,
-  setUserBoards,
-}) => {
-  const { isModalOpen, openModal, closeModal } = useModal();
-  const [isEditBoardModalOpen, setIsEditBoardModalOpen] = useState(false);
+export const MyBoards = ({ icon, boards, selectedItem, choice, setUserBoards }) => {
+	const { isModalOpen, openModal, closeModal } = useModal();
+	const [isEditBoardModalOpen, setIsEditBoardModalOpen] = useState(false);
 
-  const modalStateSwapper = () => {
-    setIsEditBoardModalOpen((prev) => !prev);
-  };
+	const modalStateSwapper = () => {
+		setIsEditBoardModalOpen(prev => !prev);
+	};
 
-  const dispatch = useDispatch();
-  const deleteBoard = (id) => {
-    dispatch(deleteDashboardThunk(id));
-  };
+	const dispatch = useDispatch();
+	const deleteBoard = id => {
+		dispatch(deleteDashboardThunk(id));
+	};
 
-  return (
-    <div>
-      <Heading>My boards</Heading>
-      <CreateBoardWrapper>
-        <p className="createText">Create a new board</p>
-        <ButtonCreateBoard onClick={() => openModal()} type="submit">
-          <svg className="iconCreateBoard" width="20px" height="20px">
-            <use href={`${icon}#icon-plus`}></use>
-          </svg>
-        </ButtonCreateBoard>
-      </CreateBoardWrapper>
-      <BoardList>
-        {boards.map((board) => (
-          <BoardItem
-            key={board._id}
-            onClick={() => choice(board)}
-            className={selectedItem._id === board._id ? "active" : ""}
-          >
-            <NavLink to={`/home/${board.title}`}>
-              <BoardContainer>
-                <BoardIcon
-                  className="iconBoard iconActive"
-                  width="20px"
-                  height="20px"
-                >
-                  <use href={`${icon}${board.icon}`}></use>
-                </BoardIcon>
-                <BoardTitle>{board.title}</BoardTitle>
-              </BoardContainer>
-            </NavLink>
+	return (
+		<div>
+			<Heading>My boards</Heading>
+			<CreateBoardWrapper>
+				<p className='createText'>Create a new board</p>
+				<ButtonCreateBoard onClick={() => openModal()} type='submit'>
+					<svg className='iconCreateBoard' width='20px' height='20px'>
+						<use href={`${icon}#icon-plus`}></use>
+					</svg>
+				</ButtonCreateBoard>
+			</CreateBoardWrapper>
 
-            {selectedItem.id === board.id && (
-              <ButtonContainer>
-                <ButtonEdit onClick={() => modalStateSwapper()} type="button">
-                  <BoardIcon
-                    className="iconBoard iconEdit"
-                    width="16px"
-                    height="16px"
-                  >
-                    <use href={`${icon}#icon-pencil`}></use>
-                  </BoardIcon>
-                </ButtonEdit>
-                <ButtonEdit
-                  onClick={() => deleteBoard(board._id)}
-                  type="button"
-                >
-                  <BoardIcon
-                    className="iconBoard iconEdit"
-                    width="16px"
-                    height="16px"
-                  >
-                    <use href={`${icon}#icon-trash`}></use>
-                  </BoardIcon>
-                </ButtonEdit>
-              </ButtonContainer>
-            )}
-          </BoardItem>
-        ))}
-      </BoardList>
-      <AddBoardModal
-        setUserBoards={setUserBoards}
-        isModalOpen={isModalOpen}
-        modalStateSwapper={closeModal}
-      />
-      <EditBoardModal
-        isModalOpen={isEditBoardModalOpen}
-        modalStateSwapper={modalStateSwapper}
-      />
-    </div>
-  );
+			{boards.length !== 0 && (
+				<BoardList>
+					{boards.map(board => (
+						<BoardItem
+							key={board._id}
+							onClick={() => choice(board)}
+							className={selectedItem._id === board._id ? "active" : ""}>
+							<Link to={`/home/${board.title}`}>
+								<BoardContainer>
+									<BoardIcon className='iconBoard iconActive' width='20px' height='20px'>
+										<use href={`${icon}${board.icon}`}></use>
+									</BoardIcon>
+									<BoardTitle>{board.title}</BoardTitle>
+								</BoardContainer>
+								{console.log(selectedItem._id)}
+								{selectedItem.id === board.id && (
+									<ButtonContainer>
+										<ButtonEdit onClick={() => modalStateSwapper()} type='button'>
+											<BoardIcon className='iconBoard iconEdit' width='16px' height='16px'>
+												<use href={`${icon}#icon-pencil`}></use>
+											</BoardIcon>
+										</ButtonEdit>
+										<ButtonEdit onClick={() => deleteBoard(board._id)} type='button'>
+											<BoardIcon className='iconBoard iconEdit' width='16px' height='16px'>
+												<use href={`${icon}#icon-trash`}></use>
+											</BoardIcon>
+										</ButtonEdit>
+									</ButtonContainer>
+								)}
+							</Link>
+						</BoardItem>
+					))}
+				</BoardList>
+			)}
+
+			<AddBoardModal setUserBoards={setUserBoards} isModalOpen={isModalOpen} modalStateSwapper={closeModal} />
+			<EditBoardModal isModalOpen={isEditBoardModalOpen} modalStateSwapper={modalStateSwapper} />
+		</div>
+	);
 };
