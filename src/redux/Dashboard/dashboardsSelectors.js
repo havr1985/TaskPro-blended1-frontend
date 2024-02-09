f17599cf93a8545437bf00d462c3d6f192046db2
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2024-02-09 08:29:02 +0200

    pul or

--- src/redux/Dashboard/dashboardsSelectors.js
+++ src/redux/Dashboard/dashboardsSelectors.js
@@ -1,3 +1,4 @@
-export const selectAllDashboards = state => state.dashboards.dashboards;
+export const selectAllDashboards = (state) => state.dashboards.dashboards;
 
-export const selectCurrentDashboard = state => state.dashboards.currentDashboard;
+export const selectCurrentDashboard = (state) =>
+  state.dashboards.currentDashboard;
