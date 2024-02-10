682d72b3f093b634704cab9825bdae5894c8ebda
Alona <polejcukalona@gmail.com>
2024-02-10 18:05:49 +0200

    add change status popup

---
+++ src/components/dashboard/ChangeStatusModal/ChangeStatusModal.jsx
@@ -0,0 +1,35 @@
+import icons from "../../../shared/images/icons.svg";
+import { Icon } from "../Card/Card.styled";
+import { ShowAllBtn, ModalFilter} from "./ChangeStatusModal.styled"
+import { selectCurrentDashboard } from "../../../redux/Dashboard/dashboardsSelectors";
+import { useSelector } from "react-redux";
+
+export const ChangeStatusModal = ({ isOpen, onClose, onSelectStatus, cardId }) => {
+    const { column: columns } = useSelector(selectCurrentDashboard);
+
+  if (!isOpen) return null;
+
+  const handleStatus = (columnId) => {
+    onClose();
+    onSelectStatus(columnId, cardId);
+  }
+  
+    return (
+        <ModalFilter>
+            {columns === undefined ? (
+          <></>
+        ) : (
+          columns.map(({ _id: id, title, }) => {
+            return (
+                <ShowAllBtn key={id} onClick={() => handleStatus(id)}>
+                    {title}
+                    <Icon>
+                    <use href={icons + "#icon-arrow-circle-broken-right"} />
+                    </Icon>
+                </ShowAllBtn>
+            );
+          })
+        )}
+      </ModalFilter>
+    );
+  };
---
+++ src/components/dashboard/ChangeStatusModal/ChangeStatusModal.styled.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { Icon } from "../Card/Card.styled";
+
+export const ModalFilter = styled.div`
+  position: absolute;
+  top: 40px;
+  right: 24px;
+  height: auto;
+  padding: 15px;
+  border: 1px solid var(--shared-input-border-color);
+  box-shadow: 0px 4px 16px var(--color-shadow);
+  border-radius: 8px;
+  background-color: var(--primary-background-color);
+  z-index: 1000;
+  min-width: 160px;
+`
+
+export const ShowAllBtn = styled.button`
+    background-color: transparent;
+    color: var(--screens-page-tutorial-text-color);
+    font-size:12px;
+    border: none;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    
+    &:hover {
+      color: var(--primary-button-color);
+      
+      & ${Icon} {
+        stroke: var(--primary-button-color);
+        scale: 1.1;
+      }
+  }
+`
