254a7de02b2a528f7183832f0db0a6295363a46f
Yurii <131679817+havr1985@users.noreply.github.com>
2024-02-11 21:23:27 +0200

    fix gh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TaskPro-blended1-frontend',
+  base: '/TaskPro-blended1-frontend/',
   plugins: [react()],
 })
 // import { defineConfig } from "vite";
