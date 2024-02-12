c310756d8e143463d45bc4ff28cfda09b8b7ed15
Oleh <oleh.tsarevych@gmail.com>
2024-02-12 12:45:11 +0200

    sidebar done

--- src/components/Sidebar/CreateNewBoard/CreateNewBoard.styled.js
+++ src/components/Sidebar/CreateNewBoard/CreateNewBoard.styled.js
@@ -51,7 +51,7 @@ export const ButtonCreateBoard = styled.button`
 	background: var(--sidebar-button-create-background);
 	border-radius: 6px;
 
-	transition: 0.5s;
+	transition: all 450ms cubic-bezier(0.4, 0, 0.2, 1);
 
 	&:hover {
 		background: var(--plus-button-hover-color);
--- src/components/Sidebar/LogOut/LogOut.styled.js
+++ src/components/Sidebar/LogOut/LogOut.styled.js
@@ -1,30 +1,30 @@
 import styled from "styled-components";
 
 export const ButtonLogOut = styled.button`
-  width: 105px;
-  height: 32px;
+	width: 105px;
+	height: 32px;
 
-  border: transparent;
-  background: none;
-  padding: 0;
+	border: transparent;
+	background: none;
+	padding: 0;
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 
-  cursor: pointer;
+	cursor: pointer;
 
-  font-size: 16px;
-  font-weight: 500;
-  line-height: 24px;
-  letter-spacing: 0em;
-  color: var(--secondary-text-sidebar-color);
-  transition: all 450ms cubic-bezier(0.4, 0, 0.2, 1);
-  .iconLogOut {
-    stroke: var(--logout-button-color);
-    fill: var(--sidebar-icon-fill-color);
-  }
-  &:hover .iconLogOut {
-    stroke: var(--logout-hover-button-color);
-  }
+	font-size: 16px;
+	font-weight: 500;
+	line-height: 24px;
+	letter-spacing: 0em;
+	color: var(--secondary-text-sidebar-color);
+	transition: all 450ms cubic-bezier(0.4, 0, 0.2, 1);
+	.iconLogOut {
+		stroke: var(--logout-button-color);
+		fill: var(--sidebar-icon-fill-color);
+	}
+	&:hover .iconLogOut {
+		stroke: var(--logout-hover-button-color);
+	}
 `;
